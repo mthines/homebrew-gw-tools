3beeaec8017e96d87758302ae2d6fd23055c5740
mthines <mads.thines@dash0.com>
2026-02-02 12:59:20 +0100

    fix: rename formula from gw@beta to gw-beta (@ is for numeric versions only)

--- Formula/gw-beta.rb
+++ Formula/gw-beta.rb
@@ -1,4 +1,4 @@
-class GwATBeta < Formula
+class GwBeta < Formula
   desc "Git worktree manager - Streamline your multi-branch development workflow (Beta)"
   homepage "https://github.com/mthines/gw-tools"
   version "0.13.4-beta.1"
