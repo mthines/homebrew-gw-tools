c370b65d074fb731d2ab3c1d988d8ece6b3c9770
mthines <mads.thines@dash0.com>
2026-02-02 12:56:13 +0100

    gw@beta: update to v0.13.4-beta.1

--- Formula/gw@beta.rb
+++ Formula/gw@beta.rb
@@ -1,16 +1,16 @@
 class GwATBeta < Formula
   desc "Git worktree manager - Streamline your multi-branch development workflow (Beta)"
   homepage "https://github.com/mthines/gw-tools"
-  version "0.13.3-beta.1"
+  version "0.13.4-beta.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mthines/gw-tools/releases/download/v0.13.3-beta.1/gw-macos-arm64"
-      sha256 "677e64c62ef585e147a3030391fdd8a3a6ed60c9e8f8721c3ba05473eb8c4a65"
+      url "https://github.com/mthines/gw-tools/releases/download/v0.13.4-beta.1/gw-macos-arm64"
+      sha256 "09c1dc647cd94dc9ffeab468b14e7a371f3b26407caf29f8e9b32b9697fed30d"
     else
-      url "https://github.com/mthines/gw-tools/releases/download/v0.13.3-beta.1/gw-macos-x64"
-      sha256 "7b181a5f89b4f7f5a7b782fc03f1f922b95a3496d43d262a0945ce0639f924fc"
+      url "https://github.com/mthines/gw-tools/releases/download/v0.13.4-beta.1/gw-macos-x64"
+      sha256 "1d6a9641bc6f64e1a77c38d9c5b28cd145b44b80c4808f670f76c9f52b44b029"
     end
   end
 
