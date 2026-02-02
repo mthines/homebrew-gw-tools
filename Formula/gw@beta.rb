803f0cd0d281c1df3b2b7ae8470155fdb5d005af
mthines <mads.thines@dash0.com>
2026-02-02 10:46:54 +0100

    gw@beta: add initial beta formula

---
+++ Formula/gw@beta.rb
@@ -0,0 +1,25 @@
+class GwAT < Formula
+  desc "Git worktree manager - Streamline your multi-branch development workflow (Beta)"
+  homepage "https://github.com/mthines/gw-tools"
+  version "0.0.0-beta.1"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/mthines/gw-tools/releases/download/v0.0.0-beta.1/gw-macos-arm64"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/mthines/gw-tools/releases/download/v0.0.0-beta.1/gw-macos-x64"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "gw-macos-arm64" => "gw" if Hardware::CPU.arm?
+    bin.install "gw-macos-x64" => "gw" if Hardware::CPU.intel?
+  end
+
+  test do
+    assert_match "gw version", shell_output("#{bin}/gw --version")
+  end
+end
